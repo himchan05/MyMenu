64eca600612d7b060f99459a79b522909c8041ed
박힘찬 <himchan05@naver.com>
2022-03-19 12:09:42 +0900

    Initial Commit

---
+++ MyMenu/MyMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMenuApp.swift
+//  MyMenu
+//
+//  Created by 박힘찬 on 2022/03/19.
+//
+
+import SwiftUI
+
+@main
+struct MyMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
